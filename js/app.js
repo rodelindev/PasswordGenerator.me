e707c90f970a41840cd438bad0a399f7e90287bd
rodelindipre96 <rdbivieca@gmail.com>
2021-04-20 23:52:43 -0400

    Inicializando el repositorio.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function() {
+
+    /*Variables y objetos generados*/
+
+    //Formulario de la app
+    var app = document.getElementById('app');
+
+    //Numero de caracteres de la contraseña
+    var inputCaracteres = document.getElementById('numero-caracteres');
+    
+    
+    
+    
+}());
